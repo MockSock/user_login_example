e83060b460b29a9fbbf796ee22b3e27d7fe422cc
Austin Martz <business.mocksock@gmail.com>
2023-04-25 18:51:24 -0500

    Add a size variable
    I added a size variable that uses MediaQuery in order to dynamically size my widgets

--- lib/widgets/sign-up.dart
+++ lib/widgets/sign-up.dart
@@ -13,6 +13,10 @@ class _SignUpState extends State<SignUp> {
     final _formKey = GlobalKey<FormState>();
     final _email = TextEditingController();
     final _password = TextEditingController();
+
+    // This will be for sizing
+    final _size = MediaQuery.of(context).size;
+
     return Form(
       key: _formKey,
       child: Container(
