fd2a820d6af35b7a5432c5bdfdf345c393b84548
Austin Martz <business.mocksock@gmail.com>
2023-04-25 18:22:04 -0500

    Add basics of a Stateful Widget
    I added a SignUp stateful widget that will soon be turned into a form for users to sign up with

--- lib/widgets/sign-up.dart
+++ lib/widgets/sign-up.dart
@@ -1,2 +1,15 @@
 // This will store the widget information for a sign up screen
 
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('I work!');
+  }
+}
